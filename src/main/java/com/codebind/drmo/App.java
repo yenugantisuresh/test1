53086e5fa549aa1a74f584aa338de30f722e9758
root <root@ip-172-31-23-36.us-east-2.compute.internal>
2018-02-08 11:28:52 +0000

    message

---
+++ src/main/java/com/codebind/drmo/App.java
@@ -0,0 +1,13 @@
+package com.codebind.drmo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
